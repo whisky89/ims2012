c26d5187fc6bd9803a42f5381315d5a91c37a2cb
whisky <p.whisky@gmail.com>
2012-12-01 18:46:39 +0000

    vytvoren ARCHIV, plny preklad do CZ, kontrola, komentare, mistopredsedkyne wait !!!MRKNOUT NA NAVRH - na zacatku souboru

--- uohs.cc
+++ uohs.cc
@@ -1,6 +1,14 @@
 /*
  * OPTIMALIZACE: priorita u elektronicke posty - efektivnejsi vyuziti casu 
  */
+
+/*
+ * NAVRH K MONITOROVANI ZPRAV - pridat typ zpravy a pocitadla techto zprav
+ *  -meli by jsme k dispozici informaci o tom, kolik zprav kterych typu 
+ *   bylo uspesne zpracovano a pote archivovano
+ */
+
+
 #include <simlib.h> 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +25,7 @@
 #define PODATELNA_TEXT 4 * 60
 #define PODATELNA_DATA 4 * 60
 
-// assistenti
+// asistenti
 #define ASSISTENT_ZPRACOVANI 2 * 60
 #define ASSISTENT_ZALOZENI 2 * 60
 
@@ -35,17 +43,19 @@
 // referenti
 #define REF_ZPRACOVANI 21 * 24 * 60
 
+//#define debug
 
 Facility podatelna1("1. pracovnik na podatelne");
 Facility podatelna2("2. pracovnik na podatelne");
-Facility assistent1("Asistent 1"); //prvni asistent ,queueAssisten1
-Facility assistent2("Asistent 2"); //druhy asistent ,queueAssisten2
+Facility asistent1("Asistent 1"); //prvni asistent ,queueAssisten1
+Facility asistent2("Asistent 2"); //druhy asistent ,queueAssisten2
 Facility mistopredsedkyne ("Mistopredsedkyne"); //
 Facility reditel[5]; //pocet reditelu - na kazdy odbor jeden
-Store referent[5]; //pocet oddeleni (pro kazde oddeleni 8 referentu)
 
 
-Facility isWorkingTime ("Je pracovni doba"); // pracovni doba
+//Facility isWorkingTime ("Je pracovni doba"); // pracovni doba
+
+Queue archiv;
 
 int cnt=0;
 int verejne=0;
@@ -57,25 +67,28 @@ int el=0;
 
 
 /**
- * Denni pracovni doba
+ * Simulace denni pracovni doby
+ * 
+ * Pracovni doba je stanovena na 8 hodin denne
+ * (neuvazujeme prescasy, svatky ci dovolene)
  */
-class WorkingTime : public Process{
+class PracovniDoba : public Process{
   void Behavior(){
 //    Priority=3;
     while(1){
       Wait(8*60); //pracovni doba 8hodin
-      Seize(isWorkingTime,3);
+//      Seize(isWorkingTime,3);
       Seize(podatelna1,3);
       Seize(podatelna2,3);
-      Seize(assistent1,3);
-      Seize(assistent2,3);
+      Seize(asistent1,3);
+      Seize(asistent2,3);
       Seize(mistopredsedkyne,3);
       Wait(16*60);//nepracuje se
-      Release(isWorkingTime);
+//      Release(isWorkingTime);
       Release(podatelna1);
       Release(podatelna2);
-      Release(assistent1);
-      Release(assistent2);
+      Release(asistent1);
+      Release(asistent2);
       Release(mistopredsedkyne);
     }
   }
@@ -84,172 +97,199 @@ class WorkingTime : public Process{
 /**
  * Rodicovska trida pro jednotlive typy zprav
  */
-class Message : public Process {
+class Zprava : public Process {
   protected:
     /**
-      * popis chovani zpracovani na podatelne
-      * @param waitTime - doba zpracovani zpavy
+      * Popis chovani zpracovani na podatelne
+      * @param cas - doba zpracovani zpavy
       */
-    void podatelnaBehavior(double waitTime){
-      // pro rovnomerne zatizeni pracovniku
+    void podatelnaBehavior(double cas){
+      // rovnomerne zatizeni pracovniku na podatelne
       if(podatelna1.QueueLen()<podatelna2.QueueLen()){
         Seize(podatelna1);
-        Wait(Exponential(waitTime));
+        Wait(Exponential(cas));
         Release(podatelna1);
       } else if(podatelna1.QueueLen()>podatelna2.QueueLen()){
         Seize(podatelna2);
-        Wait(Exponential(waitTime));
+        Wait(Exponential(cas));
         Release(podatelna2);
       }else{
         if(Random()<0.50){
           Seize(podatelna1);
-          Wait(Exponential(waitTime));
+          Wait(Exponential(cas));
           Release(podatelna1);
         }else{
           Seize(podatelna2);
-          Wait(Exponential(waitTime));
+          Wait(Exponential(cas));
           Release(podatelna2);
         }
       }
+#ifdef debug
+      Print("PODATELNA - zpracovano v: %lf\n",Time);      
+#endif
     }
-    // popis zbytku chovani v systemu
+    /**
+     *  Popis zbytku systemu
+     */
     void Behavior(){
       double percent;
-      if(assistent1.QueueLen() < assistent2.QueueLen()){
-        Seize(assistent1);
+      if(asistent1.QueueLen() < asistent2.QueueLen()){
+        Seize(asistent1);
         Wait(Exponential(2*60));
-        Release(assistent1);
-      } else if(assistent1.QueueLen() > assistent2.QueueLen()){
-        Seize(assistent2);
+        Release(asistent1);
+      } else if(asistent1.QueueLen() > asistent2.QueueLen()){
+        Seize(asistent2);
         Wait(Exponential(2*60));
-        Release(assistent2);
+        Release(asistent2);
       } 
       else {
         if(Random()<0.50) {
-          Seize(assistent2);
+          Seize(asistent2);
           Wait(Exponential(2*60));
-          Release(assistent2);
+          Release(asistent2);
         } else {
-          Seize(assistent1);
+          Seize(asistent1);
           Wait(Exponential(2*60));
-          Release(assistent1);
+          Release(asistent1);
           
         }
-      }  
+      }
+#ifdef debug
+      Print("ASISTENT-PRIJEM - zpracovano v: %lf\n",Time);      
+#endif
 
       Seize(mistopredsedkyne);
-      Wait(5);
+      //prideleni na konkretni odbor na zaklade predchozi analyzy pozadavku
+//      Wait(5); 
+      Wait(Exponential(5));
       Release(mistopredsedkyne);
       
       percent = Random();
       
-      if(percent <= 0.07){
-        //Odbor druhostupnoveho rozhodovani 7%
-        //rozh++;
+      if(percent <= 0.07){ //Odbor druhostupnoveho rozhodovani 7%
+        rozh++;
         //TODO: REDITELOVA PRACE
         Wait(60*21*24);//cca 21 dni kolobeh zpracovani zprav
-        if(assistent1.QueueLen() < assistent2.QueueLen()){
-          Seize(assistent1);
+        if(asistent1.QueueLen() < asistent2.QueueLen()){
+          Seize(asistent1);
           Wait(Exponential(2*60));
-          Release(assistent1);
-        } else if(assistent1.QueueLen() >= assistent2.QueueLen()){
-          Seize(assistent2);
+          Release(asistent1);
+        } else if(asistent1.QueueLen() >= asistent2.QueueLen()){
+          Seize(asistent2);
           Wait(Exponential(2*60));
-          Release(assistent2);
+          Release(asistent2);
         }
       }
-      else if(percent <= 0.17){
-        //verejna podpora 10%
-//        Print("podpora\n");
-        //podpora++;
+      else if(percent <= 0.17){//verejna podpora 10%
+        podpora++;
         Wait(60*21*24);
-        Seize(assistent2,1);
-        Wait(Exponential(2*60));        
-        Release(assistent2);
+        Seize(asistent2,1);
+        Wait(Exponential(2*60));     
+        Release(asistent2);
       }
-      else if(percent <= 0.29){
-//        //ekonomicky odbor 12%
-////        Print("eko\n");
-//        eko++;
+      else if(percent <= 0.29){//ekonomicky odbor 12%
+        eko++;
         Wait(60*21*24);
-        Seize(assistent1,1);
+        Seize(asistent1,1);
         Wait(Exponential(2*60));
-        Release(assistent1);
+        Release(asistent1);
       }
-      else if(percent <= 0.51){
-//        //hospodarska soutez 22%
-////        Print("soutez\n");
-//        soutez++;
-
+      else if(percent <= 0.51){//hospodarska soutez 22%
+        soutez++;
         Wait(60*21*24);
-        Seize(assistent2,1);     
+        Seize(asistent2,1);     
         Wait(Exponential(2*60));
-      
-        Release(assistent2);
+        Release(asistent2);
 //        Print("uvolneni asistenta\n");
       }
-      else{
-//        //verejne zakazky 49%
-//        verejne++;
+      else{//verejne zakazky 49%    
+        verejne++;
         Wait(60*21*24);
-        Seize(assistent1,1);
-        Wait(Exponential(2*60));    
-        Release(assistent1);
+        Seize(asistent1,1);
+        Wait(Exponential(2*60));
+        Release(asistent1);
 //        Print("uvolneni asistenta\n");
-      }    
+      }
+      archiv.InsLast(this);
+      Passivate();
     }
 
 };
 
-class WrittenMessage : public Message {
+/**
+ * Specifikace Zpravy
+ * Pisemna zprava
+ */
+class PisemnaZprava : public Zprava {
   void Behavior(){
     cnt++;
-    Message::podatelnaBehavior(60 * 4);
-//    Print("posledni: %lf\n",Time);
-    Message::Behavior();
+    Zprava::podatelnaBehavior(60 * 4);
+    Zprava::Behavior();
   }
 };
 
-class DataMessage : public Message {
+/**
+ * Specifikace Zpravy
+ * Datova zprava
+ */
+class DatovaZprava : public Zprava {
   void Behavior(){
     cnt++;
-    Message::podatelnaBehavior(60 * 4);
-//    Print("posledni: %lf\n",Time);
-    Message::Behavior();
+    Zprava::podatelnaBehavior(60 * 4);
+    Zprava::Behavior();
     
   }
 };
 
-class ElectronicMessage : public Message {
+/**
+ * Specifikace Zpravy
+ * Elektronicka zprava (e-mail)
+ */
+class ElektronickeZprava : public Zprava {
   void Behavior(){
     cnt++;
     el++;
     Priority=2;
-    Message::podatelnaBehavior(5);
-//    Print("posledni: %lf\n",Time);
+    Zprava::podatelnaBehavior(5);
     Priority=0;
-    Message::Behavior();
+    Zprava::Behavior();
  
   }
 };
 
-class GeneratorWrtMsg : public Event {
+
+/* ***************************************** *
+ *            Generatory udalosti            * 
+ * ***************************************** */
+
+/**
+ * Generovani prichodu pisemnych zprav do systemu
+ */
+class GeneratorPisemnaZprava : public Event {
   void Behavior(){
-	 (new WrittenMessage)->Activate();
+	 (new PisemnaZprava)->Activate();
     Activate(Time + Exponential(84));
   }
 };
 
-class GeneratorDataMsg : public Event {
+
+/**
+ * Generovani prichodu datovych zprav do systemu
+ */
+class GeneratorDatovaZprava : public Event {
   void Behavior(){
-	 (new DataMessage)->Activate();
+	 (new DatovaZprava)->Activate();
     Activate(Time + Exponential(108)); //108
   }
 };
 
-class GeneratorElMsg : public Event {
+
+/**
+ * Generovani prichodu zprav v elektronicke podobe do systemu
+ */
+class GeneratorElZprava : public Event {
   void Behavior(){
-	 (new ElectronicMessage)->Activate();
+	 (new ElektronickeZprava)->Activate();
     Activate(Time + Exponential(120));
   }
 };
@@ -257,62 +297,41 @@ class GeneratorElMsg : public Event {
 int main(int argc, char **argv)
 {
   Init(0,43829.1); // mesic behu
-//  Init(0,9*60); //10 hodin behu
+//  Init(0,15*60); //10 hodin behu
 //  Init(0,7*24*60); // tyden behu
 //  Init(0,1051897.44);//2roky
-//  std::string ttrf;
   
-//  mistopredsedkyne ("fronta mistopredsedkyne");
-
+  archiv.SetName("Archiv vyrizenych zadosti");
   reditel[0].SetName("Reditel verejnych zakazek");
   reditel[1].SetName("Reditel hospodarske souteze");
   reditel[2].SetName("Reditel ekonomickeho odboru");
   reditel[3].SetName("Reditel verejne podpory");
   reditel[4].SetName("Reditel odboru druhostupnoveho rozhodovani");
-  
-  referent[0].SetName("Referent verejnych zakazek"); 
-  referent[1].SetName("Referent hospodarske souteze");
-  referent[2].SetName("Referent ekonomickeho odboru");
-  referent[3].SetName("Referent verejne podpory");
-  referent[4].SetName("Referent odboru druhostupnoveho rozhodovani");
-  
-//  for(int i=0;i<5;i++){ //nastaveni kapacit referentu
-//    referent[i].SetCapacity(8);
-//    for(int j=0;j<8;j++){
-//      referent[i].SetQueue(referentQueue[i][j]);
-//      ttrf= "";
-//      ttrf.append(referent[i].Name());
-//      ttrf.append(" c.");
-//      ttrf.append(1,(j+'0'));
-//      referentQueue[i][j].SetName(ttrf.c_str());
-//    }
-//  }
-  
 
-  (new WorkingTime)->Activate();
-  (new GeneratorWrtMsg)->Activate();
-  (new GeneratorDataMsg)->Activate();
-  (new GeneratorElMsg)->Activate();
+  (new PracovniDoba)->Activate();
+  (new GeneratorPisemnaZprava)->Activate();
+  (new GeneratorDatovaZprava)->Activate();
+  (new GeneratorElZprava)->Activate();
   Run();
   
   
   podatelna1.Output();
   podatelna2.Output();
-  Print("prichozich zprav: %d\n",cnt);
-  Print("prichozich elektronickych zprav: %d\n",el);
-//  assistent1.Output();
-//  assistent2.Output();
+//  Print("prichozich elektronickych zprav: %d\n",el);
+//  asistent1.Output();
+//  asistent2.Output();
 //  mistopredsedkyne.Output();
+//  archiv.Output();
+#ifdef debug
+  SIMLIB_statistics.Output();
 
-
+  Print("Pocet zprav na odbor verejnych zakazek %d\n",verejne);
+  Print("Pocet zprav na odbor hospodarske souteze: %d\n",soutez);
+  Print("Pocet zprav na ekonomicky odbor: %d\n",eko);
+  Print("Pocet zprav na odbor verejne podpory: %d\n",podpora);
+  Print("Pocet zprav na odboru druhostupnoveho rozhodovani: %d\n",rozh);
+#endif
+  Print("Prichozich zprav celkem: %d\n",cnt);
   
-//  referentQueue[1][1].Output();
-    
- // Print("verejne zakazky %d\n",verejne);
- // Print("hosp. soutez %d\n",soutez);
- // Print("eko %d\n",eko);
- // Print("podpora %d\n",podpora);
- // Print("rozhodovani %d\n",rozh);
-// qFilOffice.Output();
   return 0;
 }
